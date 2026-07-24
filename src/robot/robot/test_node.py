d1dc7ab1d5ae61d40e39438f58fc8cade7f13727
germangiraffe <germangiraffe@ROS.myguest.virtualbox.org>
2026-07-24 18:53:56 -0400

    Initial setup of pkg

---
+++ src/robot/robot/test_node.py
@@ -0,0 +1,17 @@
+import rclpy
+from rclpy.node import Node
+
+class TestNode(Node):
+    def __init__(self):
+        super().__init__('test_node')
+        self.get_logger().info('Test Success')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TestNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ src/robot/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='germangiraffe',
+    maintainer_email='alexanderwiegand310@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'test_node = robot.test_node:main',
+        ],
+    },
+)
